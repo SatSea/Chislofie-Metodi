3cab1751303ad8c5e62103506dbb032c5fc8baec
SatSea <staspetrov388@gmail.com>
2022-01-25 12:18:54 +0300

    Update Main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-import
+import math
+
+# input 
+A = input()
+B = input()
+E = input()
+
+
+#def for func
+def f(x):
+ return math.exp(-x)-(0,5*math.sin(x)^2)
+
+
+#from block diagram
+
+while ((A-B)<E):
+    X = (A+B)/2
+    if ():
+        B = X
+    else:
+        A = X
+
+print(X)
+
+
