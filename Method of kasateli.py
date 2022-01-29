f7b7047a44d6bd9c4a9207568bf33a61a0e1c0a3
SatSea <staspetrov388@gmail.com>
2022-01-29 11:23:11 +0300

    Metod of kasatelnoi

--- Method of kasateli.py
+++ Method of kasateli.py
@@ -1,1 +1,19 @@
+#функция
+def F(x):
+    return 0.1*math.pow(x,2)-x*math.log(x)
+ 
+#производная
+def F1(x):
+    return 0.2*x-math.log(x)-1
+ 
+ 
+def Method(a,b):
+    x0=(a+b)/2
+    xn=F(x0)
+    xn1=xn-F(xn)/F1(xn)
+    while abs(xn1-xn)>math.pow(10,-5):
+        xn=xn1 
+        xn1=xn-F(xn)/F1(xn)
+    return xn1
 
+Print(Method(input(), input()))
