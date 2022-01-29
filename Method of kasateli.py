c20a809b99962e1639c48fe3f368700fa46cfe62
SatSea <staspetrov388@gmail.com>
2022-01-29 11:41:51 +0300

    Not working

--- Method of kasateli.py
+++ Method of kasateli.py
@@ -1,7 +1,6 @@
 import math
 
 
-
 #функция
 def F(x):
     return 0.1*math.pow(x,2)-x*math.log(x)
@@ -19,21 +18,4 @@ def Method(a,b):
         xn=xn1 
         xn1=xn-F(xn)/F1(xn)
     return xn1
-
-def Get_root():
-    a = input()
-    b = input()
-    h = input()
-
-    x1 = a
-    x2 = x1 + h
-    y1 = f(x1)
-
-    while(x2<b):
-        y2 = f(x2)
-        if (y1*y2 =< 0):
-            print(x1, x2)
-        x1 = x2
-        x2 = x1+h
-        y1=y2
 
