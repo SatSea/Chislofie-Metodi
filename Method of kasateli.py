60f25c84f195d3f3ea05cd8f5ff2ca7f0fca7dcf
SatSea <staspetrov388@gmail.com>
2022-01-30 15:29:28 +0300

    Make some commenting and refactoring to easier reading code (maybe to fix it :^) )

--- Method of kasateli.py
+++ Method of kasateli.py
@@ -1,21 +1,48 @@
 import math
 
+# input 
+a = float(input()) # lower interval
+b = float(input()) # higher interval
+e = float(input()) # accuracy
 
 #функция
-def F(x):
-    return 0.1*math.pow(x,2)-x*math.log(x)
+def f(x):
+    return math.exp(-x)-(math.sin(x)**2)/2
  
-#производная
-def F1(x):
-    return 0.2*x-math.log(x)-1
- 
- #сам метод
-def Method(a,b):
-    x0=(a+b)/2
-    xn=F(x0)
-    xn1=xn-F(xn)/F1(xn)
-    while abs(xn1-xn)>math.pow(10,-5):
-        xn=xn1 
-        xn1=xn-F(xn)/F1(xn)
-    return xn1
 
+#производная
+def f1(x):
+    return -(math.exp(-x))-math.sin(2*x)/2
+
+
+#сам метод
+def Method(a,b,e):
+    x0=(a+b)/2 
+    xn=f(x0)
+    xn1=xn-f(xn)/f1(xn)
+    while abs(xn1-xn)>e:
+        xn=xn1 
+        xn1=xn-f(xn)/f1(xn)
+    print(f"Найден корень %.4f с погрешностью {e}" % xn1)
+
+
+#отделение корней
+def root_separation(a,b,e):
+    count = 0
+    try:
+        x1 = a
+        x2 = x1 + e
+        y1 = f(x1)
+        while(x2 < b):
+            y2 = f(x2)
+            if (y1 * y2 <= 0):
+                Method(a,b,0.0001)
+                count += 1
+            x1 = x2
+            x2 = x1 + e
+            y1 = y2
+        print(f"Поиск завершен\nНайдено {count} корней")
+    except:
+        print("Что-то пошло не так" )
+
+root_separation(a,b,e)
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import math
 
 # input 
-a = float(input())
-b = float(input())
-e = float(input())
+a = float(input()) # lower interval
+b = float(input()) # higher interval
+e = float(input()) # accuracy
 
 
 #def for func
@@ -11,21 +11,41 @@ def f(x):
  return math.exp(-x)-(math.sin(x)**2)/2
 
 
-x=a
-while ((b-a) > e):
+def half_division_method(a,b,e):
+    x=a
+    while ((b-a) > e):
 
-    x = (a+b)/2
+        x = (a+b)/2
 
-    if (f(x) == 0):
-        break
+        if (f(x) == 0):
+            break
 
-    if (f(x)*f(a) < 0):
-        b = x
-    else:
-        a = x
-            
-print("Корень равен: {x}", x)
-
-print(x)
+        if (f(x)*f(a) < 0):
+            b = x
+        else:
+            a = x
+    
+    print("Найден корень: %.3f" % (x) )   
 
 
+#отделение корней
+def root_separation(a,b,e):
+    count = 0
+    try:
+        x1 = a
+        x2 = x1 + e
+        y1 = f(x1)
+        while(x2 < b):
+            y2 = f(x2)
+            if (y1 * y2 <= 0):
+                half_division_method(a,b,0.0001)
+                count += 1
+            x1 = x2
+            x2 = x1 + e
+            y1 = y2
+        print(f"Поиск завершен \nНайдено {count} корней")
+    except:
+        print("Что-то пошло не так" )
+
+
+root_separation(a,b,e)
