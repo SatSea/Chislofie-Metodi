31c22d39fcf7dcd6dd1dd14d72e0378e781f8d18
SatSea <staspetrov388@gmail.com>
2022-01-31 13:38:51 +0300

    Bebra88

--- metod hord.py
+++ metod hord.py
@@ -1,30 +1,47 @@
 import math
 
-def F(x):
-    return 0.1 * math.pow(x, 2) - x * math.log(x)
+#функция
+def f(x):
+    return math.exp(-x)-(math.sin(x)**2)/2
+ 
 
-def F1(x):
-    res=0.2 * x - math.log(x) - 1
-    print(res)
-    return res
+#производная
+def f1(x):
+    return -(math.exp(-x))-math.sin(2*x)/2
 
-def Method(a, b):
+
+def Method(a, b, e):
     try:
         x0 = (a + b) / 2
-        xn = F(x0)
-        xn1 = xn - F(xn) / F1(xn)
-        while abs(xn1 - xn) > math.pow(10, -5):
+        xn = f(x0)
+        xn1 = xn - f(xn) / f1(xn)
+        while abs(xn1 - xn) > e:
             xn = xn1 
-            xn1 = xn - F(xn) / F1(xn)
-            print(xn1)
-        return xn1
+            xn1 = xn - f(xn) / f1(xn)
+        print("Корень равен: %.3f" % xn1)
     except ValueError:
         print("Значение не получилось")
 
 
-x=float(input())
 a=float(input())
 b=float(input())
-F(x)
-F1(x)
-Method(a, b)
+e=float(input())
+
+def root_separation(a,b,e):
+    count = 0
+    x1 = a
+    x2 = x1 + e
+    y1 = f(x1)
+    while(x2 < b):
+        y2 = f(x2)
+        if (y1 * y2 <= 0):
+            print(x1,x2)
+            Method(x1,x2,0.0001)
+            count += 1
+        x1 = x2
+        x2 = x1 + e
+        y1 = y2
+    print(f"Поиск завершен\nНайдено {count} корней")
+
+root_separation(a,b,e)
+
